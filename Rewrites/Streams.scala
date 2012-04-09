515a638405f47891ed56260f5a24395c8324c564
Olivier Chafik <olivier.chafik@gmail.com>
2012-04-09 17:13:37 +0100

    added todo to Streams rewrites

--- Rewrites/Streams.scala
+++ Rewrites/Streams.scala
@@ -3,6 +3,7 @@ package scalaxy; package rewrites
 import Macros._
 
 object Streams {
+  // TODO add conditions macro + isSideEffectFree(f)
   def mapMap[A, B, C](col: Seq[A], f: A => B, g: B => C) = Replacement(
     col.map(f).map(g),
     col.map(a => {
