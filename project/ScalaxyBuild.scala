87ecefcb4fe1f85c05d848eec472821db1914a2b
Olivier Chafik <olivier.chafik@gmail.com>
2012-10-27 20:33:14 +0100

    Cleanup of build file

--- project/ScalaxyBuild.scala
+++ project/ScalaxyBuild.scala
@@ -4,9 +4,10 @@ import Keys._
 
 object Scalaxy extends Build
 {
-  override lazy val settings = super.settings ++ Seq(
-    shellPrompt := { s => Project.extract(s).currentProject.id + "> " }
-  ) ++ scalaSettings
+  override lazy val settings = 
+    super.settings ++ Seq(
+      shellPrompt := { s => Project.extract(s).currentProject.id + "> " }
+    ) ++ scalaSettings
 
   lazy val standardSettings =
     Defaults.defaultSettings ++
@@ -14,24 +15,21 @@ object Scalaxy extends Build
     compilationSettings ++
     mavenSettings ++
     scalaSettings ++
-    commonDepsSettings
+    testSettings
 
   lazy val infoSettings = Seq(
     organization := "com.nativelibs4java",
     version := "0.3-SNAPSHOT",
     licenses := Seq("BSD-3-Clause" -> url("http://www.opensource.org/licenses/BSD-3-Clause")),
-    homepage := Some(url("http://ochafik.com/blog/"))
-  )
+    homepage := Some(url("http://ochafik.com/blog/")))
+
   lazy val compilationSettings = Seq(
     scalacOptions ++= Seq("-encoding", "UTF-8", "-deprecation", "-unchecked"),
-    javacOptions ++= Seq("-Xlint:unchecked")
-  )
+    javacOptions ++= Seq("-Xlint:unchecked"))
+
   lazy val mavenSettings = Seq(
     publishMavenStyle := true,
-
     resolvers += Resolver.sonatypeRepo("snapshots"),
-    //publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository")))
-
     publishTo <<= version { (v: String) =>
       val nexus = "https://oss.sonatype.org/"
       if (v.trim.endsWith("-SNAPSHOT"))
@@ -39,17 +37,16 @@ object Scalaxy extends Build
       else
         Some("releases"  at nexus + "service/local/staging/deploy/maven2")
     },
-    pomIncludeRepository := { _ => false }
-  )
+    pomIncludeRepository := { _ => false })
+
   lazy val scalaSettings = Seq(
-    scalaVersion := "2.10.0-RC1"
-    //crossScalaVersions := Seq("2.10.0-M2"),
+    //crossScalaVersions := Seq("2.10.0-RC2"),
     //exportJars := true, // use jars in classpath
-  )
-  lazy val commonDepsSettings = Seq(
+    scalaVersion := "2.10.0-RC1")
+
+  lazy val testSettings = Seq(
     libraryDependencies += "junit" % "junit" % "4.10" % "test",
-    libraryDependencies += "com.novocode" % "junit-interface" % "0.8" % "test"
-  )
+    libraryDependencies += "com.novocode" % "junit-interface" % "0.8" % "test")
 
   lazy val scalaxy =
     Project(id = "scalaxy", base = file("."), settings = standardSettings ++ Seq(
@@ -69,20 +66,30 @@ object Scalaxy extends Build
     dependsOn(core, macros, compilets)
 
   lazy val core =
-    Project(id = "scalaxy-core", base = file("Core"), settings = standardSettings ++ Seq(
-      libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _),
-      libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-reflect" % _)
-    )).
+    Project(
+      id = "scalaxy-core",
+      base = file("Core"),
+      settings =
+        standardSettings ++ Seq(
+          libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _),
+          libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-reflect" % _)
+        )).
     dependsOn(macros)
 
   lazy val compilets =
-    Project(id = "scalaxy-compilets", base = file("Compilets"), settings = standardSettings).
+    Project(
+      id = "scalaxy-compilets",
+      base = file("Compilets"),
+      settings = standardSettings).
     dependsOn(macros)
 
   lazy val macros =
-    Project(id = "scalaxy-macros", base = file("Macros"), settings = standardSettings ++ Seq(
-      libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _),
-      //libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-reflect" % _),
-      scalacOptions ++= Seq("-language:experimental.macros")
-    ))
+    Project(
+      id = "scalaxy-macros",
+      base = file("Macros"),
+      settings =
+        standardSettings ++ Seq(
+          libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _),
+          scalacOptions ++= Seq("-language:experimental.macros")
+        ))
 }
