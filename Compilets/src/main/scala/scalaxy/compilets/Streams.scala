df316688b89f22b2064e17892b0938f28cfe71f4
Olivier Chafik <olivier.chafik@gmail.com>
2012-11-18 15:00:12 +0000

    Prepare streams tests

--- Compilets/src/main/scala/scalaxy/compilets/Streams.scala
+++ Compilets/src/main/scala/scalaxy/compilets/Streams.scala
@@ -2,9 +2,10 @@ package scalaxy; package compilets
 
 object Streams extends Compilet {
   // TODO add conditions macro + isSideEffectFree(f)
-  def mapMap[A, B, C](col: Seq[A], f: A => B, g: B => C) = replace(
+  def mapMap[A, B, C](col: List[A], f: A => B, g: B => C) = replace(
     col.map(f).map(g),
     col.map(a => {
+      //g(f(a))
       val b = f(a)
       val c = g(b)
       c
