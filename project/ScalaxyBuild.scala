09df4ebfa97bf16d5507b74c774d61e036a7da67
Olivier Chafik <olivier.chafik@gmail.com>
2013-01-07 18:26:27 +0000

    Switched to Scala 2.10.0

--- project/ScalaxyBuild.scala
+++ project/ScalaxyBuild.scala
@@ -7,7 +7,7 @@ object Scalaxy extends Build
 {
   lazy val scalaSettings = Seq(
     //exportJars := true, // use jars in classpath
-    scalaVersion := "2.10.0-RC2",
+    scalaVersion := "2.10.0",
     //scalaVersion := "2.11.0-SNAPSHOT",
     crossScalaVersions := Seq(
       "2.11.0-SNAPSHOT"))
