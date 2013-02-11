836b48081deefbcddd0b667b302e59848c2843df
Olivier Chafik <olivier.chafik@gmail.com>
2013-02-11 17:57:52 +0000

    Update Fx/Runtime/src/main/scala/scalaxy/fx/runtime/ScalaChangeListener.scala

--- Fx/Runtime/src/main/scala/scalaxy/fx/runtime/ScalaChangeListener.scala
+++ Fx/Runtime/src/main/scala/scalaxy/fx/runtime/ScalaChangeListener.scala
@@ -6,6 +6,9 @@ import javafx.beans.property._
 import javafx.beans.value._
 import javafx.event._
 
+/** This class is needed at runtime because it is currently impossible to reify it from a macro.
+ *  Related Scala bug: https://issues.scala-lang.org/browse/SI-6386
+ */
 abstract class ScalaChangeListener[T] extends ChangeListener[T] {
   override def changed(observable: ObservableValue[_ <: T], oldValue: T, newValue: T) {
     changed(oldValue, newValue)
