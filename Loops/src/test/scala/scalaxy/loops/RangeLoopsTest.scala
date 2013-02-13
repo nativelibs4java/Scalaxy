af0942dcef778f4955b59d06ea0f2728d99edee0
Olivier Chafik <olivier.chafik@gmail.com>
2013-02-13 09:54:48 +0000

    More tests

--- Loops/src/test/scala/scalaxy/loops/RangeLoopsTest.scala
+++ Loops/src/test/scala/scalaxy/loops/RangeLoopsTest.scala
@@ -4,14 +4,16 @@ import org.junit._
 import org.junit.Assert._
 
 import scala.collection.mutable.ArrayBuffer
+import scala.reflect.ClassTag
+
 import scalaxy.loops._
 
 class LoopsTest 
 {
   private val end = 10
   private val start = 3
-  private def withBuf(f: ArrayBuffer[Int] => Unit): List[Int] = {
-    val buf = ArrayBuffer[Int]()
+  private def withBuf[T : ClassTag](f: ArrayBuffer[T] => Unit): List[T] = {
+    val buf = ArrayBuffer[T]()
     f(buf)
     buf.toList
   }
@@ -23,13 +25,13 @@ class LoopsTest
     val o = 5
     val p = 2
     assertEquals(
-      withBuf(res =>
+      withBuf[Int](res =>
         for (i <- 0 until n)
           for (j <- 0 until m)
             for (k <- 0 until o)
               for (l <- 0 until p)
                 res += (i * 1 + j * 10 + k * 100 + l * 1000) / 10),
-      withBuf(res =>
+      withBuf[Int](res =>
         for (i <- 0 until n optimized)
           for (j <- 0 until m optimized)
             for (k <- 0 until o optimized)
@@ -40,10 +42,10 @@ class LoopsTest
   @Test
   def simpleRangeUntil {
     assertEquals(
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- start until end) 
           res += (i * 2)),
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- start until end optimized) 
           res += (i * 2)))
   }
@@ -51,10 +53,10 @@ class LoopsTest
   @Test
   def simpleRangeTo {
     assertEquals(
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- start to end) 
           res += (i * 2)),
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- start to end optimized) 
           res += (i * 2)))
   }
@@ -62,23 +64,23 @@ class LoopsTest
   @Test
   def simpleRangeUntilBy {
     assertEquals(
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- start until end by 2) 
           res += (i * 2)),
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- start until end by 2 optimized) 
           res += (i * 2)))
     // This should be empty:
     assertEquals(
       Nil,
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- start until end by -2 optimized) 
           res += (i * 2)))
     assertEquals(
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- end until start by -2) 
           res += (i * 2)),
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- end until start by -2 optimized) 
           res += (i * 2)))
   }
@@ -86,24 +88,59 @@ class LoopsTest
   @Test
   def simpleRangeToBy {
     assertEquals(
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- start to end by 2) 
           res += (i * 2)),
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- start to end by 2 optimized) 
           res += (i * 2)))
     // This should be empty:
     assertEquals(
       Nil,
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- start to end by -2 optimized) 
           res += (i * 2)))
     assertEquals(
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- end to start by -2) 
           res += (i * 2)),
-      withBuf(res => 
+      withBuf[Int](res => 
         for (i <- end to start by -2 optimized) 
           res += (i * 2)))
+  }
+  
+  @Test
+  def stableRangeIndexCapture {
+    assertEquals(
+      withBuf[() => Int](res => 
+        for (i <- start to end by 2) 
+          res += (() => (i * 2))).map(_()),
+      withBuf[() => Int](res => 
+        for (i <- start to end by 2 optimized) 
+          res += (() => (i * 2))).map(_()))
+  }
+  
+  @Test
+  def nameCollisions {
+    val n = 10
+    val m = 3
+    assertEquals(
+      withBuf[Int](res =>
+        for (i <- 0 until n) {
+          for (j <- 0 until m) {
+            val i = j
+            res += i * 100 + j
+          }
+        }
+      ),
+      withBuf[Int](res =>
+        for (i <- 0 until n optimized) {
+          for (j <- 0 until m optimized) {
+            val i = j
+            res += i * 100 + j
+          }
+        }
+      )
+    )
   }
 }
