8d8ea380a5d1ae9b12b8b83ea9362eeb3dc2742d
Olivier Chafik <ochafik@google.com>
2013-07-09 21:41:37 +0100

    Reified: fixed basic captures

--- Reified/src/main/scala/scalaxy/package.scala
+++ Reified/src/main/scala/scalaxy/package.scala
@@ -12,14 +12,14 @@ package object reified
   def reify[A, B](f: A => B): ReifiedFunction[A, B] = macro reified.impl.reifyFunction[A, B]
   
   def reify[A](v: A): ReifiedValue[A] = macro reified.impl.reifyValue[A]
-  
-  private[reified] def capture(index: Int) = ???
 }
 
 package reified 
 {
   object impl 
   {
+    def capture[T](index: Int): T = ???
+    
     def reifyFunction[A : c.WeakTypeTag, B : c.WeakTypeTag](c: Context)(f: c.Expr[A => B]): c.Expr[ReifiedFunction[A, B]] = {
       val (tree, capturesExpr) = transformReifiedRefs(c)(c.typeCheck(f.tree))
       val expr = c.Expr[runtime.universe.Expr[A => B]](
@@ -48,51 +48,108 @@ package reified
       })
     }
     
-    def transformReifiedRefs(c: Context)(tree: c.universe.Tree): (c.universe.Tree, c.Expr[Array[AnyRef]]) = {
+    def transformReifiedRefs(c: Context)(tree: c.universe.Tree): (c.universe.Tree, c.Expr[Seq[AnyRef]]) = {
       import c.universe._
       import definitions._
       
+      val localDefSyms = collection.mutable.HashSet[Symbol]()
+      def isDefLike(t: Tree) = t match {
+        case Function(_, _) => true
+        case _ if t.isDef => true
+        case _ => false
+      }
+      (new Traverser {
+        override def traverse(t: Tree) {
+          super.traverse(t)
+          if (t.symbol != null && isDefLike(t)) {
+            localDefSyms += t.symbol
+          }
+        }
+      }).traverse(tree)
+      
+      var lastCaptureIndex = -1
+      val capturedTerms = collection.mutable.ArrayBuffer[Tree]()
+      val capturedSymbols = collection.mutable.HashMap[TermSymbol, Int]()
+      
+      //println(s"localDefSyms = $localDefSyms")
       val transformer = new Transformer {
         override def transform(t: Tree): Tree = {
-          if (t.symbol != null) {
+          if (t.symbol != null && !isDefLike(t)) {
             val sym = t.symbol
-            println(s"transform($t { symbol = ${sym}, isFreeTerm = ${sym.isFreeTerm}, owner = ${sym.owner}, isMethod = ${sym.isMethod}, isLocal = ${sym.isLocal} })")
-            if (sym.isTerm && !t.isDef) {
+            //println(s"transform($t { tpe = ${t.tpe}, symbol = ${sym}, isFreeTerm = ${sym.isFreeTerm}, owner = ${sym.owner}, isMethod = ${sym.isMethod}, isLocal = ${sym.isLocal} })")
+            if (sym.isTerm && !localDefSyms.contains(sym)) {
               val tsym = sym.asTerm
               if (tsym.isVar) {
                 c.error(t.pos, "Cannot capture a var")
-              //} else if (!tsym.isStable) {
-              //  c.error(t.pos, "This reference is not stable, cannot capture it.")
+                t
               } else if (tsym.isLazy) {
                 c.error(t.pos, "Cannot capture lazy vals")
-              } else if (tsym.isMethod && !tsym.isGetter && !tsym.isAccessor) {
-                // TODO: Do nothing... if supported method 
-              } else {
-                t.tpe match {
-                  case tr @ TypeRef(_, _, List(vtpe)) 
-                  if tr <:< typeOf[ReifiedValue[_]] =>
-                    c.error(t.pos, "TODO: handle capture of reified terms")
-                  case _ =>
-                    c.error(t.pos, "Capture of this expression is not supported")
+                t
+              } else if (tsym.isVal || tsym.isAccessor) {
+                val captureIndexExpr = c.literal(
+                  capturedSymbols.get(tsym) match {
+                    case Some(i) =>
+                      i
+                    case None =>
+                      lastCaptureIndex += 1
+                      capturedSymbols += tsym -> lastCaptureIndex
+                      capturedTerms += Ident(tsym)//t
+                      
+                      lastCaptureIndex
+                  }
+                )
+                
+                val tpe = t.tpe.normalize.widen
+                //println(s"tpe = $tpe: ${tpe.getClass.getName}")
+                //println(s"symbol.tpe = ${sym.typeSignature}
+                // Abuse reify to get correct reference to `capture`.
+                val Apply(TypeApply(f, List(_)), args) = {
+                  reify(reified.impl.capture[Int](captureIndexExpr.splice)).tree
                 }
-              }
+                c.typeCheck(
+                  Apply(TypeApply(f, List(TypeTree(tpe))), args),
+                  tpe
+                )
+              } else if (tsym.isMethod) {
+                super.transform(t)
+              } else {
+                c.error(t.pos, s"Capture of this expression is not supported (symbol = $tsym)")
+                t
+              } 
+            } else {
+              super.transform(t)
             }
+          } else {
+            super.transform(t)
           }
-          super.transform(t)
         }
       }
       
-      println(s"Transforming $tree")
-      val res = transformer.transform(tree)
-      println(s"Result: $res")
-      (res, reify(Array[AnyRef]()))
+      //println(s"Transforming $tree")
+      val transformed = transformer.transform(tree)
+      //println(s"Result: $transformed")
+      
+      val arrExpr = {
+        // Abuse reify to get correct seq constructor
+        val Apply(seqConstructor, _) = reify(Seq[AnyRef]()).tree
+        c.Expr[Seq[AnyRef]](
+          c.typeCheck(
+            Apply(
+              seqConstructor,
+              capturedTerms.map(term => {
+                val termExpr = c.Expr[Any](term)
+                reify(termExpr.splice.asInstanceOf[AnyRef]).tree
+              }).toList),
+            c.typeOf[Seq[AnyRef]]))
+      }
+      (transformed, arrExpr)
     }
   }
   
   class ReifiedValue[A](
       val value: A,
       private[reified] val rawExpr: runtime.universe.Expr[A],
-      private[reified] val captures: Array[AnyRef]) {
+      val captures: Seq[AnyRef]) {
     val expr = {
       // TODO: replace captures
       rawExpr
@@ -102,7 +159,7 @@ package reified
   class ReifiedFunction[A, B](
     f: A => B,
     rawExpr: runtime.universe.Expr[A => B],
-    captures: Array[AnyRef])
+    captures: Seq[AnyRef])
       extends ReifiedValue[A => B](f, rawExpr, captures)
       with Function1[A, B] {
       
--- Reified/src/test/scala/scalaxy/reified/ReifiedTest.scala
+++ Reified/src/test/scala/scalaxy/reified/ReifiedTest.scala
@@ -3,7 +3,7 @@ package scalaxy.debug.test
 import org.junit._
 import org.junit.Assert._
 
-import scalaxy.reified._
+import scalaxy.reified.{ reify, ReifiedValue, ReifiedFunction }
 
 class ReifiedTest {
   
@@ -12,11 +12,14 @@ class ReifiedTest {
     val x = 10
     val r = reify(10 * x)
     assertTrue(r.isInstanceOf[ReifiedValue[_]])
+    assertEquals(Seq(10), r.captures.toSeq)
   }
   
   @Test
   def testFunction = {
-    val r = reify((x: Int) => x * x)
+    val x = 10
+    val r = reify((y: Int) => x * y)
     assertTrue(r.isInstanceOf[ReifiedFunction[_, _]])
+    assertEquals(Seq(10), r.captures.toSeq)
   }
 }
