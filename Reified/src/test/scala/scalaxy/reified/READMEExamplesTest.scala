9fd6a1386dcc9e063083fb674ce36e8b6f9b2d0b
Olivier Chafik <olivier.chafik@gmail.com>
2013-08-03 20:17:20 +0100

    Reified: fixed + tested example integrator

--- Reified/src/test/scala/scalaxy/reified/READMEExamplesTest.scala
+++ Reified/src/test/scala/scalaxy/reified/READMEExamplesTest.scala
@@ -4,6 +4,7 @@ import org.junit._
 import org.junit.Assert._
 
 import scala.reflect.runtime.universe
+import scala.reflect.runtime.universe.TypeTag
 import scala.reflect.runtime.currentMirror
 
 import scalaxy.reified._
@@ -33,36 +34,54 @@ class READMEExamplesTest extends TestUtils with PerfTestUtils {
     }
   }
 
-  @Test
-  def testDiscreteIntegrator {
-    import scalaxy.reified._
-    def createDiscreteIntegrator(f: ReifiedValue[(Double, Double) => Double], step: Double = 0.1): ReifiedValue[(Double, Double, Double, Double) => Double] = {
-      (xMin: Double, xMax: Double, yMin: Double, yMax: Double) =>
-        {
-          var tot = 0.0
-          val iMin = (xMin / step).toInt
-          val iMax = (xMax / step).toInt
-          val jMin = (yMin / step).toInt
-          val jMax = (yMax / step).toInt
-          for (i <- iMin until iMax) {
-            val x = i * step;
-            for (j <- jMin until jMax) {
-              val y = j * step
-              tot += f(x, y)
-            }
+  import scalaxy.reified._
+  def createDiscreteIntegrator2D(f: ReifiedValue[(Double, Double) => Double], step: Double): ReifiedValue[(Double, Double, Double, Double) => Double] = {
+    (xMin: Double, xMax: Double, yMin: Double, yMax: Double) =>
+      {
+        val nx = ((xMax - xMin) / step).toInt
+        val ny = ((yMax - yMin) / step).toInt
+
+        var sum = 0.0
+        val halfStep = step / 2
+        var x = xMin + halfStep
+        var y = yMin + halfStep
+        for (i <- 0 to nx) {
+          for (j <- 0 to ny) {
+            sum += f(x, y)
+            y += step
           }
-          tot
+          x += step
         }
-    }
+        step * sum
+      }
+  }
+  def createDiscreteIntegrator1D(f: ReifiedValue[Double => Double], step: Double): ReifiedValue[(Double, Double) => Double] = {
+    (xMin: Double, xMax: Double) =>
+      {
+        val nx = ((xMax - xMin) / step).toInt
+
+        var sum = 0.0
+        var x = xMin + step / 2
+        for (i <- 0 to nx) {
+          sum += f(x)
+          x += step
+        }
+        step * sum
+      }
+  }
+
+  @Test
+  def testDiscreteIntegrator2D {
+
     import scala.math._
 
     val factor = 2.0
     val f = reify((x: Double, y: Double) => {
       cos(x * factor) - sin(y / factor)
     })
-    val fIntegrator = createDiscreteIntegrator(f)
-    //val fIntegrator = createDiscreteIntegrator((v: Double) => f(shift(v)))
-    //val fIntegrator = createDiscreteIntegrator(f)
+    val fIntegrator = createDiscreteIntegrator2D(f, 0.01)
+    //val fIntegrator = createDiscreteIntegrator2D((v: Double) => f(shift(v)))
+    //val fIntegrator = createDiscreteIntegrator2D(f)
 
     println("fIntegrator.taggedExpr = " + fIntegrator.taggedExpr)
     println("fIntegrator.capturedTerms = " + fIntegrator.capturedTerms)
@@ -84,32 +103,34 @@ class READMEExamplesTest extends TestUtils with PerfTestUtils {
     }))
   }
 
-  @Test
-  def testDiscreteConvolver {
-    import scalaxy.reified._
-    def createDiscreteConvolver(
-      f: ReifiedValue[Double => Double],
-      g: ReifiedValue[Double => Double],
-      step: Double = 0.1): ReifiedValue[(Double, Double) => Double] = {
+  import scalaxy.reified._
+  def createDiscreteConvolver1D(
+    f: ReifiedValue[Double => Double],
+    g: ReifiedValue[Double => Double],
+    step: Double = 0.1): ReifiedValue[(Double, Double) => Double] = {
 
-      (xMin: Double, xMax: Double) =>
-        {
-          var tot = 0.0
-          val iMin = (xMin / step).toInt
-          val iMax = (xMax / step).toInt
-          for (i <- iMin until iMax) {
-            val x = i * step;
-            tot += f(x) * g(xMax - (x - xMin))
-          }
-          tot
+    (xMin: Double, xMax: Double) =>
+      {
+        var sum = 0.0
+        val iMin = (xMin / step).toInt
+        val iMax = (xMax / step).toInt
+        for (i <- iMin until iMax) {
+          val x = i * step;
+          sum += f(x) * g(xMax - (x - xMin))
         }
-    }
+        step * sum
+      }
+  }
+
+  @Test
+  def testDiscreteConvolver2D {
+
     import scala.math._
 
     val factor = 1 / Pi
     val f = reify(cos(_))
     val g = reify(sin(_))
-    val fgConvolver = createDiscreteConvolver(f, g)
+    val fgConvolver = createDiscreteConvolver1D(f, g)
 
     println("fgConvolver.taggedExpr = " + fgConvolver.taggedExpr)
     println("fgConvolver.capturedTerms = " + fgConvolver.capturedTerms)
@@ -127,6 +148,45 @@ class READMEExamplesTest extends TestUtils with PerfTestUtils {
 
     compare("testDiscreteConvolver", n, iterations)(reify(() => {
       fgConvolver(0, 10) + fgConvolver(20, 30) + fgConvolver(30, 40)
+    }))
+  }
+
+  def assertEqualsRelativePrecision(expected: Double, actual: Double, relativePrecision: Double) {
+    assertEquals(expected, actual, Math.abs(expected * relativePrecision))
+  }
+
+  @Test
+  def poly {
+    val f = reify((x: Double) => 1 + x * (2 + x * (3 + x * 2))) // 1 + 2x + 3x^2 + 2x^3
+    val fDerivate = reify((x: Double) => 2 + x * (6 + x * 6))
+    val fPrimitive = (x: Double) => x * (1 + x * (2 + x * (1 + x * 1 / 2.0)))
+
+    val xMin = 0
+    val xMax = 10
+    val step = 0.01
+    val relativePrecision = 0.02
+
+    val fIntegrator = createDiscreteIntegrator1D(f, step)
+    val fCompiledIntegrator = fIntegrator.compile()()
+
+    // Check integral
+    assertEqualsRelativePrecision(
+      fPrimitive(xMax) - fPrimitive(xMin),
+      fCompiledIntegrator(xMin, xMax),
+      relativePrecision)
+
+    // Integrate f derivative to check
+    assertEqualsRelativePrecision(
+      f(xMax) - f(xMin),
+      createDiscreteIntegrator1D(fDerivate, step).compile()()(xMin, xMax),
+      relativePrecision)
+
+    // Compare performance
+    val n = 3000
+    val iterations = 3
+
+    compare("poly", n, iterations)(reify(() => {
+      fIntegrator(0, 10) + fIntegrator(20, 30) + fIntegrator(30, 40)
     }))
   }
 }
