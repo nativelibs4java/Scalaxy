1cac9ebd9feac022a1b4f34fcb87beccedbd2734
Olivier Chafik <olivier.chafik@gmail.com>
2013-11-02 14:25:31 +0000

    Casbah: some scaladoc

--- CasbahDSL/src/main/scala/scalaxy/casbah/internal/api.scala
+++ CasbahDSL/src/main/scala/scalaxy/casbah/internal/api.scala
@@ -7,21 +7,28 @@ import scala.util.matching.Regex
 sealed trait Doc extends Dynamic {
 
   object set extends Dynamic {
+    /** `doc.set(a = b)` => `$set(a -> b)` */
     @applyDynNamedFunc("$set")
     def applyDynamicNamed(n: String)(args: (String, Col)*): Col = ???
   }
   object rename extends Dynamic {
+    /** `doc.rename(a = b)` => `$rename(a -> b)` */
     @applyDynNamedFunc("$rename")
     def applyDynamicNamed(n: String)(args: (String, String)*): Col = ???
   }
   object setOnInsert extends Dynamic {
+    /** `doc.setOnInsert(a = b)` => `$setOnInsert(a -> b)` */
     @applyDynNamedFunc("$setOnInsert")
     def applyDynamicNamed(n: String)(args: (String, Col)*): Col = ???
   }
 
   @peel def selectDynamic(name: String): Col = ???
+  /** `doc.a = b` => `$set(a -> b)` */
   @updateDynFunc("$set") def updateDynamic(name: String)(value: Col): Col = ???
+  /** `doc -= a` => `$unset(a)` */
   @func1("$unset") def -=(n: String): Col = ???
+  /**  `doc.contains(k)` => `k $exists true`
+   *  `!doc.contains(k)` => `k $exists false` */
   @testMeth1("$exists") def contains(key: String): BoolCol = ???
 }
 
@@ -36,16 +43,24 @@ sealed trait Col extends AnyRef {
   @op("$all") def all(values: Col*): BoolCol = ???
   @op("$in") def in(values: Col*): BoolCol = ???
   @op("$where") def where(js: String): BoolCol = ???
-  @op("$regex") def matches(js: String): BoolCol = ???
+  @op("$regex") def matches(r: String): BoolCol = ???
+  @op("$regex") def matches(r: Regex): BoolCol = ???
+  /** `a & b` => `$bit(a) and b` */
   @funcOp("$bit", "and") def &(b: Col): Col = ???
+  /** `a | b` => `$bit(a) or b` */
   @funcOp("$bit", "or") def |(b: Col): Col = ???
+  /** `a % b == c` => `a $mod (b, c)` */
   @testOp1("$mod") def %(b: Col): Col = ???
+  /** `a.size == b` => `a $size b` */
   @testOp0("$size") def size(): Col = ???
+  /** `a += b` => `$inc(a -> b)` */
   @opFunc("$minus$greater", "$inc") def +=(b: Col): Col = ???
 }
 
 sealed trait BoolCol extends AnyRef {
+  /** `a && b` => `$and(a, b)` */
   @func2("$and") def &&(b: BoolCol): BoolCol = ???
+  /** `a || b` => `$or(a, b)` */
   @func2("$or") def ||(b: BoolCol): BoolCol = ???
   def unary_! : BoolCol = ???
 }
