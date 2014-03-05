6c2caba08423b67ff345e86dcca4aca94e0ae319
Olivier Chafik <olivier.chafik@gmail.com>
2014-03-05 23:31:21 +0000

    Loops: added ArrayBufferSink

---
+++ Loops/Loops-2.11/src/main/scala/streams/sinks/ArrayBufferSinks.scala
@@ -0,0 +1,29 @@
+package scalaxy.loops
+
+private[loops] trait ArrayBufferSinks extends StreamSources {
+  val global: scala.reflect.api.Universe
+  import global._
+
+  case object ArrayBufferSink extends StreamSink
+  {
+    override def sinkOption = Some(this)
+
+    override def outputNeeds = Set(RootTuploidPath)
+
+    override def emitSink(
+        inputVars: TuploidValue[TermName],
+        fresh: String => TermName,
+        transform: Tree => Tree): StreamOpResult =
+    {
+      val builder = fresh("builder")
+      require(inputVars.alias.nonEmpty, s"inputVars = $inputVars")
+
+      StreamOpResult(
+        // TODO pass source collection to canBuildFrom if it exists.
+        prelude = List(q"val $builder = scala.collection.mutable.ArrayBuffer[${inputVars.tpe}]()"),
+        body = List(q"$builder += ${inputVars.alias.get}"),
+        ending = List(q"$builder.result()")
+      )
+    }
+  }
+}
