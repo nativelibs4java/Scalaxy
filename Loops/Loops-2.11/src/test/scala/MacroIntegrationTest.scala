43094f2da93ac45ffde76e753e4bd97adc94e922
Olivier Chafik <olivier.chafik@gmail.com>
2014-03-07 22:41:51 +0000

    Loops: parameterized macro integ test

--- Loops/Loops-2.11/src/test/scala/MacroIntegrationTest.scala
+++ Loops/Loops-2.11/src/test/scala/MacroIntegrationTest.scala
@@ -3,16 +3,30 @@ package scalaxy.loops
 package test
 
 import org.junit._
-import Assert._
+import org.junit.Assert._
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
 
-class MacroIntegrationTest extends StreamComponentsTestBase with StreamOps {
+@RunWith(classOf[Parameterized])
+class MacroIntegrationTest(source: String) extends StreamComponentsTestBase with StreamOps {
+  @Test def test {
+    assertMacroCompilesToSameValue(source)
+  }
+}
+
+object MacroIntegrationTest extends StreamComponentsTestBase with StreamOps {
   import toolbox.u._
 
-  @Test def testArrayMapFilter {
-    assertMacroCompilesToSameValue("Array(1, 2, 3).map(_ * 2).filter(_ < 3)")
-  }
-
-  @Test def testRangeMapFilter {
-    assertMacroCompilesToSameValue("(1 to 10).map(_ * 2).filter(_ < 3)")
+  @Parameters(name = "{0}")
+  def data: java.util.Collection[Array[AnyRef]] = {
+    import collection.JavaConversions._
+    List(
+      "Array(1, 2, 3).map(_ * 2).filter(_ < 3)",
+      "(2 to 10).map(_ * 2).filter(_ < 3)",
+      "(2 until 10 by 2).map(_ * 2)",
+      "(20 to 7 by -3).map(_ * 2).filter(_ < 3)",
+      "Array(1, 2, 3).map(_ * 2).map(_ < 3)"
+    ).map(Array(_: AnyRef))
   }
 }
