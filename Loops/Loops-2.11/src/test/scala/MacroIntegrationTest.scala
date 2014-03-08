af280e63b88743671abf8b4c744505175e295515
Olivier Chafik <olivier.chafik@gmail.com>
2014-03-08 01:39:44 +0000

    Loops: a couple more tests with captures

--- Loops/Loops-2.11/src/test/scala/MacroIntegrationTest.scala
+++ Loops/Loops-2.11/src/test/scala/MacroIntegrationTest.scala
@@ -8,25 +8,23 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
 
+import scala.collection.JavaConversions._
+
 @RunWith(classOf[Parameterized])
 class MacroIntegrationTest(source: String) extends StreamComponentsTestBase with StreamOps {
-  @Test def test {
-    assertMacroCompilesToSameValue(source)
-  }
+  @Test def test = assertMacroCompilesToSameValue(source)
 }
 
 object MacroIntegrationTest extends StreamComponentsTestBase with StreamOps {
-  import toolbox.u._
-
-  @Parameters(name = "{0}")
-  def data: java.util.Collection[Array[AnyRef]] = {
-    import collection.JavaConversions._
-    List(
-      "Array(1, 2, 3).map(_ * 2).filter(_ < 3)",
-      "(2 to 10).map(_ * 2).filter(_ < 3)",
-      "(2 until 10 by 2).map(_ * 2)",
-      "(20 to 7 by -3).map(_ * 2).filter(_ < 3)",
-      "Array(1, 2, 3).map(_ * 2).map(_ < 3)"
-    ).map(Array(_: AnyRef))
-  }
+  @Parameters(name = "{0}") def data: java.util.Collection[Array[AnyRef]] = List[AnyRef](
+    "Array(1, 2, 3).map(_ * 2).filter(_ < 3)",
+    "(2 to 10).map(_ * 2).filter(_ < 3)",
+    "(2 until 10 by 2).map(_ * 2)",
+    "(20 to 7 by -3).map(_ * 2).filter(_ < 3)",
+    "Array(1, 2, 3).map(_ * 2).map(_ < 3)",
+    "(10 to 20).map(i => () => i).map(_())",
+    "(10 to 20).map(_ + 1).map(i => () => i).map(_())",
+    "(10 to 20).map(_ * 10).map(i => () => i).reverse.map(_())",
+    "for ((v, i) <- (20 until 0 by -2).zipWithIndex) yield (v + i)"
+  ).map(Array(_))
 }
