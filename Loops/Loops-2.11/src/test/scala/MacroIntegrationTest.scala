62976ba2d44017836e32b7f7f95d8cd62c340362
Olivier Chafik <olivier.chafik@gmail.com>
2014-03-09 00:29:55 +0000

    Loops: test deeper nesting of flatMaps

--- Loops/Loops-2.11/src/test/scala/MacroIntegrationTest.scala
+++ Loops/Loops-2.11/src/test/scala/MacroIntegrationTest.scala
@@ -28,33 +28,63 @@ object MacroIntegrationTest
         "a pure expression does nothing in statement position; you may be omitting necessary parentheses"))
 
   @Parameters(name = "{0}") def data: java.util.Collection[Array[AnyRef]] = List[(String, CompilerMessages)](
+
     "Array(1, 2, 3).map(_ * 2).filter(_ < 3)"
       -> streamMsg("Array.map.filter -> Array", pureExpressions = 1),
+
     "Array(1, 2, 3).map(_ * 2).filterNot(_ < 3)"
       -> streamMsg("Array.map.filterNot -> Array", pureExpressions = 1),
+
     "(2 to 10).map(_ * 2).filter(_ < 3)"
       -> streamMsg("Range.map.filter -> IndexedSeq"),
+
     "(2 until 10 by 2).map(_ * 2)"
       -> streamMsg("Range.map -> IndexedSeq"),
+
     "(20 to 7 by -3).map(_ * 2).filter(_ < 3)"
       -> streamMsg("Range.map.filter -> IndexedSeq"),
+
     "Array(1, 2, 3).map(_ * 2).map(_ < 3)"
       -> streamMsg("Array.map.map -> Array", pureExpressions = 1),
+
     "(10 to 20).map(i => () => i).map(_())"
       -> streamMsg("Range.map.map -> IndexedSeq"),
+
     "(10 to 20).map(_ + 1).map(i => () => i).map(_())"
       -> streamMsg("Range.map.map.map -> IndexedSeq"),
+
     "(10 to 20).map(_ * 10).map(i => () => i).reverse.map(_())"
       -> streamMsg("Range.map.map -> IndexedSeq"),
+
     "for (p <- (20 until 0 by -2).zipWithIndex) yield p.toString"
       -> streamMsg("Range.zipWithIndex.map -> IndexedSeq", pureExpressions = 1),
+
     "for ((v, i) <- (20 until 0 by -2).zipWithIndex) yield (v + i)"
       -> streamMsg("Range.zipWithIndex.withFilter.map -> IndexedSeq", pureExpressions = 1),
+
     """Array((1, 2), (3, 4))
         .map(_ match { case p @ (i, j) => (i * 10, j / 10.0) })
         .map({ case (k, l) => k + l })"""
       -> streamMsg("Array.map.map -> Array", pureExpressions = 3),
-    "val n = 10; for (i <- 0 to n; j <- i to 1 by -1; if i % 2 == 1) yield { i + j }"
-      -> streamMsg("Range.flatMap(Range.withFilter.map) -> IndexedSeq")
+
+    """val n = 10;
+      for (i <- 0 to n;
+           j <- i to 1 by -1;
+           if i % 2 == 1)
+        yield { i + j }
+    """
+      -> streamMsg("Range.flatMap(Range.withFilter.map) -> IndexedSeq"),
+
+    """val n = 20;
+      for (i <- 0 to n;
+           j <- i to n;
+           k <- j to n;
+           l <- k to n;
+           if (i + j + k + l) % 3 == 0;
+           m <- l to n)
+        yield { i + j + k + l + m }
+    """
+      -> streamMsg("Range.flatMap(Range.flatMap(Range.flatMap(Range.withFilter.flatMap(Range.map)))) -> IndexedSeq")
+
   ).map({ case (src, msgs) => Array[AnyRef](src, msgs) })
 }
