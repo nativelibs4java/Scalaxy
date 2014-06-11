b19259b071e91a703f03132f428f97ca0728f273
Olivier Chafik <olivier.chafik@gmail.com>
2014-06-11 01:32:19 +0100

    Scalaxy: respawned Performance Tests from the dead ( https://github.com/ochafik/nativelibs4java/blob/82f9ff26684471f270f1422168888af0dd2eda40/libraries/Scalaxy/src/test/scala/scalaxy/test/PerformanceTests.scala )
    Bottom line: Scalaxy still improves a hell of a lot most operations,
    even with Scala 2.11.1 !!!

---
+++ Streams/src/test/scala/performance/CollectionPerformanceTests.scala
@@ -0,0 +1,87 @@
+package scalaxy.streams
+package test
+
+import org.junit._
+import org.junit.Assert._
+
+trait ChainedPerformanceTests {
+  this: CollectionPerformanceTests =>
+  def chain(du: (String, String)) = {
+    val (definition, use) = du
+    (definition, use + ".filter(v => (v % 2) == 0).map(_ * 2)")
+  }
+}
+
+trait NoRightTests { //extends CollectionPerformanceTests {
+  this: CollectionPerformanceTests =>
+  override def simpleScanRight = {}
+  override def simpleFoldRight = {}
+  override def simpleReduceRight = {}
+}
+trait NoScalarReductionTests {//extends CollectionPerformanceTests {
+  this: CollectionPerformanceTests =>
+  override def simpleSum = {}
+  override def simpleProduct = {}
+  override def simpleMin = {}
+  override def simpleMax = {}
+}
+class ListPerformanceTest extends CollectionPerformanceTests with NoRightTests {
+  override def col = ("val col: List[Int] = (0 to n).toList", "col")//.filter(v => (v % 2) == 0).map(_ * 2)")
+}
+class ListChainedPerformanceTest extends ListPerformanceTest with ChainedPerformanceTests {
+  override def col = chain(super.col)
+}
+class ArrayPerformanceTest extends CollectionPerformanceTests {
+  override def col = ("val col = Array.tabulate(n)(i => i)", "col")
+  @Test def simpleArrayTabulate =  if (!skip) ensureFasterCodeWithSameResult(null, "Array.tabulate(n)(i => i).toSeq")
+}
+class ArrayChainedPerformanceTest extends ArrayPerformanceTest with ChainedPerformanceTests {
+  override def col = chain(super.col)
+}
+class RangePerformanceTest extends CollectionPerformanceTests with NoRightTests with NoScalarReductionTests {
+  override def col = (null: String, "(0 until n)")
+  override def simpleToArray = {}
+  override def simpleToList = {}
+  override def simpleTakeWhile = {}
+  override def simpleDropWhile = {}
+  override def simpleSum = {}
+  override def simpleProduct = {}
+  override def simpleMin = {}
+  override def simpleMax = {}
+}
+class RangeChainedPerformanceTest extends CollectionPerformanceTests with ChainedPerformanceTests with NoRightTests with NoScalarReductionTests {
+  override def col = chain((null, "(0 until n)"))
+}
+
+trait CollectionPerformanceTests extends PerformanceTests {
+  val skip = PerformanceTests.skip
+  def col: (String, String)
+
+  /**************************
+   * Collection conversions *
+   **************************/
+  @Test def simpleToArray = if (!skip) testToArray(col)
+  @Test def simpleToList = if (!skip) testToList(col)
+  // @Ignore @Test def simpleToVector = if (!skip) testToVector(col)
+
+  @Test def simpleFilter = testFilter(col)
+  @Test def simpleFilterNot = testFilterNot(col)
+  @Ignore @Test def simpleCount = testCount(col)
+  @Ignore @Test def simpleExists = testExists(col)
+  @Ignore @Test def simpleForall = testForall(col)
+  @Ignore @Test def simpleTakeWhile = testTakeWhile(col)
+  @Ignore @Test def simpleDropWhile = testDropWhile(col)
+  @Test def simpleForeach = testForeach(col)
+  @Test def simpleMap = testMap(col)
+  @Test def simpleSum = testSum(col)
+  @Test def simpleProduct = testProduct(col)
+  @Ignore @Test def simpleMin = testMin(col)
+  @Ignore @Test def simpleMax = testMax(col)
+  @Ignore @Test def simpleScanLeft = testScanLeft(col)
+  @Ignore @Test def simpleScanRight = testScanRight(col)
+  @Ignore @Test def simpleFoldLeft = testFoldLeft(col)
+  @Ignore @Test def simpleFoldRight = testFoldRight(col)
+  @Ignore @Test def simpleReduceLeft = testReduceLeft(col)
+  @Ignore @Test def simpleReduceRight = testReduceRight(col)
+
+}
---
+++ Streams/src/test/scala/performance/PerformanceTestBase.scala
@@ -0,0 +1,167 @@
+package scalaxy.streams
+package test
+
+import org.junit._
+import org.junit.Assert._
+
+import scala.collection.mutable.ListBuffer
+import scala.tools.reflect.ToolBox
+import scala.tools.reflect.FrontEnd
+
+case class PerfRun(optimized: Boolean, output: Any, nanoTime: Long)
+
+class PerformanceTestBase extends StreamComponentsTestBase {
+
+	val perfRuns = 4
+	val defaultExpectedFasterFactor = 0.95
+
+  def testClassInfo = {
+    val testTrace = new RuntimeException().getStackTrace.filter(se => se.getClassName.endsWith("Test")).last
+    val testClassName = testTrace.getClassName
+    val methodName = testTrace.getMethodName
+    (testClassName, methodName)
+  }
+
+	type PerformanceRunner = Int => (Boolean => PerfRun)
+
+  private def getPerformanceRunner(
+  		optimized: Boolean,
+  		decls: String,
+  		code: String,
+  		sizeParam: String = "n"): PerformanceRunner =
+  {
+  	val functionCode = s"""
+  		($sizeParam: Int) => {
+  			$decls;
+  			() => ${if (optimized) optimizedCode(code) else code}
+			}
+		"""
+
+		val (compiled, messages) = compile(functionCode)
+    val sizeToExecutor = compiled().asInstanceOf[Int => (() => Any)]
+
+    (n: Int) => {
+      val executor = sizeToExecutor(n)
+      (isWarmup: Boolean) => {
+        if (isWarmup) {
+          executor()
+          null
+        } else {
+          System.gc
+          Thread.sleep(50)
+          val start = System.nanoTime
+          val output = executor().asInstanceOf[AnyRef]
+          val nanoTime = System.nanoTime - start
+          PerfRun(optimized, output = output, nanoTime = nanoTime)
+        }
+      }
+    }
+  }
+
+  def ensureFasterCodeWithSameResult(
+  		decls: String,
+  		code: String,
+  		params: Seq[Int] = Array(2, 10, 1000, 100000)/*10000, 100, 20, 2)*/,
+  		minFaster: Double = 1.0,
+  		nRuns: Int = perfRuns): Unit = {
+
+    val (testClassName, methodName) = testClassInfo
+
+    val runners @
+    	Array(
+    		optimizedRunner,
+    		normalRunner
+  		) = Array(
+	    	getPerformanceRunner(optimized = true, decls = decls, code = code),
+	    	getPerformanceRunner(optimized = false, decls = decls, code = code)
+  		)
+
+    def run = params.toList.sorted.map(param => {
+    	val testers = runners.map(_(param))
+      val firstRun = testers.map(_(false))
+
+      val Array(optimizedOutput, normalOutput) = firstRun.map(_.output)
+
+      val pref = "[" + testClassName + "." + methodName + ", n = " + param + "] "
+      if (normalOutput != optimizedOutput) {
+        fail(pref + "ERROR: Output is not the same !\n" + pref + "\t   Normal output = " + normalOutput + "\n" + pref + "\tOptimized output = " + optimizedOutput)
+      }
+
+      val runs: List[PerfRun] = firstRun.toList ++ (1 until nRuns).toList.flatMap(_ => testers.map(_(false)))
+      def calcTime(list: List[PerfRun]) = {
+        val times = list.map(_.nanoTime)
+        times.sum / times.size.toDouble
+      }
+      val (runsOptimized, runsNormal) = runs.partition(_.optimized)
+      val (timeOptimized, timeNormal) = (calcTime(runsOptimized), calcTime(runsNormal))
+
+      (param, timeNormal / timeOptimized)
+    }).toMap
+
+    // val (logName, log, properties) = Results.getLog(testClassName)
+
+    //println("Cold run...")
+    val coldRun = run
+
+    //println("Warming up...");
+    // Warm up the code being benchmarked :
+    {
+      val testers = runners.map(_(5))
+      (0 until 2500).foreach(_ => testers.foreach(_(true)))
+    };
+
+    //println("Warm run...")
+    val warmRun = run
+
+    val errors = coldRun.flatMap { case (param, coldFactor) =>
+      val warmFactor = warmRun(param)
+      //println("coldFactor = " + coldFactor + ", warmFactor = " + warmFactor)
+
+      def f2s(f: Double) = ((f * 10).toInt / 10.0) + ""
+      def printFacts(warmFactor: Double, coldFactor: Double) = {
+        val txt = methodName + "\\:" + param + "=" + Array(warmFactor, coldFactor).map(f2s).mkString(";")
+        //println(txt)
+        println(txt)
+      }
+      //def printFact(factor: Double) = log.println(methodName + "\\:" + param + "=" + f2s(factor))
+      val (expectedWarmFactor, expectedColdFactor) = {
+      	(defaultExpectedFasterFactor, defaultExpectedFasterFactor)
+        // val p = Option(properties.getProperty(methodName + ":" + param)).map(_.split(";")).orNull
+        // if (p != null && p.length == 2) {
+        //   //val Array(c) = p.map(_.toDouble)
+        //   //val c = p.toDouble; printFact(c); c
+        //   //log.print("# Test result (" + (if (actualFasterFactor >= f) "succeeded" else "failed") + "): ")
+        //   val Array(w, c) = p.map(_.toDouble)
+        //   printFacts(w, c)
+        //   (w, c)
+        // } else {
+        //   //printFact(coldFactor - 0.1); 1.0
+        //   printFacts(warmFactor - 0.1, coldFactor - 0.1)
+        //   (defaultExpectedFasterFactor, defaultExpectedFasterFactor)
+        // }
+      }
+
+      def check(warm: Boolean, factor: Double, expectedFactor: Double) = {
+        val pref = "[" + testClassName + "." + methodName + ", n = " + param + ", " + (if (warm) "warm" else "cold") + "] "
+
+        if (factor >= expectedFactor) {
+          println(pref + "  OK (" + factor + "x faster, expected > " + expectedFactor + "x)")
+          Nil
+        } else {
+          val msg = "ERROR: only " + factor + "x faster (expected >= " + expectedFactor + "x)"
+          println(pref + msg)
+          List(msg)
+        }
+      }
+
+      check(false, coldFactor, expectedColdFactor) ++
+      check(true, warmFactor, expectedWarmFactor)
+    }
+    try {
+      if (!errors.isEmpty)
+        assertTrue(errors.mkString("\n"), false)
+    } finally {
+      println()
+    }
+  }
+}
